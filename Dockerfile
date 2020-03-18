5020c1be584891a1d5841499c8d524a6303b6eb8
Mohammad Mazraeh <Mazraeh.Mohammad@Gmail.com>
2020-03-17 22:47:07 -0700

    first commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM ubuntu:18.04
+RUN apt-get update && apt-get install -y --no-install-recommends \
+	curl \
+	vim \
+	python \
+	libsdl2-2.0-0 \
+	libpython2.7 \
+	ca-certificates && \
+    rm -rf /var/lib/apt/lists/* && \
+    curl https://files.ballistica.net/bombsquad/builds/BombSquad_Server_Linux_64bit_1.4.150.tar.gz -o /tmp/BombSquad_Server_Linux_64bit_1.4.150.tar.gz && \
+    mkdir /BombSquad_Server && \
+    tar -zxvf /tmp/BombSquad_Server_Linux_64bit_1.4.150.tar.gz -C /BombSquad_Server --strip 1 && \
+    rm /tmp/BombSquad_Server_Linux_64bit_1.4.150.tar.gz
---
+++ config.py
@@ -0,0 +1,17 @@
+# place any of your own overrides here.
+# see bombsquad_server for details on what you can override
+config['partyName'] = 'Your Party Name!'
+config['partyIsPublic'] = False
+config['port'] = 43210
+config['language'] = 'English'
+config['maxPartySize'] = 6
+config['sessionType'] = 'ffa' # or teams/etc
+config['playlistCode'] = None
+config['playlistShuffle'] = True
+config['autoBalanceTeams'] = True
+config['enableTelnet'] = False
+config['telnetPort'] = 43250
+config['telnetPassword'] = 'changeme'
+config['teamsSeriesLength'] = 7
+config['ffaSeriesScoreToWin'] = 24
+config['statsURL'] = ''
